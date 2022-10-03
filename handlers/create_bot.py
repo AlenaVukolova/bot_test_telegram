885cf8c0c5f122efcfc412acd30ecea826bac15b
Алена <alenavykolova@gmail.com>
2022-10-03 11:24:57 +0000

    handlers/create_bot.py edited online with Bitbucket

--- handlers/create_bot.py
+++
@@ -1,6 +0,0 @@
-from aiogram import Bot, Dispatcher, executor, types
-
-bot = Bot(token="5579467700:AAGa1Ej4OAIxOwS0uocZ_N_Up09grAyf_AE")
-#bot = Bot(token=os.getenv("TOKEN"))
-dp = Dispatcher(bot)
-
